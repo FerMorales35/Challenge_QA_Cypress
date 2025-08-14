51d0912247b5e3face0e4e5832934b947ec75229
Fernando  Morales <fersebamorales@gmail.com>
2025-08-14 17:57:11 -0300

    ci: Cypress + Mochawesome (merge & HTML)

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,45 @@
 const { defineConfig } = require("cypress");
 const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: "https://www.saucedemo.com",
+//     //Screenshot en caso de fallar un test
+//     screenshotOnRunFailure: true,
+//     //Pantalla Maximmizada
+//     viewportWidth: 2560,
+//     viewportHeight: 1440,
+//     setupNodeEvents(on, config) {
+//       mochawesome(on);
+//     },
+//     reporter: "cypress-mochawesome-reporter",
+//     reporterOptions: {
+//       reportDir: "cypress/reports",
+//       overwrite: true,
+//       html: false,
+//       json: true,
+//       charts: true,
+//       reportPageTitle: "Reporte de Pruebas Cypress",
+//       embeddedScreenshots: true,
+//       inlineAssets: true,
+//     },
+//   },
+// });
+
+const { defineConfig } = require("cypress");
+
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    overwrite: false,
+    html: false, // generamos el HTML luego con "marge"
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    reportPageTitle: "E2E Report",
+  },
   e2e: {
     baseUrl: "https://www.saucedemo.com",
     //Screenshot en caso de fallar un test
@@ -10,16 +48,10 @@ module.exports = defineConfig({
     viewportWidth: 2560,
     viewportHeight: 1440,
     setupNodeEvents(on, config) {
-      mochawesome(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
     },
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-      reportDir: "cypress/reports",
-      overwrite: true,
-      charts: true,
-      reportPageTitle: "Reporte de Pruebas Cypress",
-      embeddedScreenshots: true,
-      inlineAssets: true,
-    },
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });
